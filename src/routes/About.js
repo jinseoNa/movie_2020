dda47fb0122786ba88e3414f3dab4427a848f0e9
unknown <jinseoNa>
2020-09-24 23:45:48 +0900

    add

---
+++ src/routes/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+    return (
+        <div className="about__container">
+            <span>
+                "일단 만들긴했는데 아직 뭐가 뭔지 1도 모르겠다"
+            </span>
+            <span>
+                -날라리 개발자-
+            </span>
+        </div>
+    );
+}
+
+export default About;
