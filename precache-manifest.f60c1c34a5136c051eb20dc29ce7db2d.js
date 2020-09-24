a5d25496f7d8d2d781b10e664cc99448215c4e5e
unknown <jinseoNa>
2020-09-24 23:47:40 +0900

    Updates

---
+++ precache-manifest.f60c1c34a5136c051eb20dc29ce7db2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1fc1510c7000ca86305e1e854e53899",
+    "url": "/movie_2020/index.html"
+  },
+  {
+    "revision": "9bb244042bf88303e1c9",
+    "url": "/movie_2020/static/css/main.731a4b71.chunk.css"
+  },
+  {
+    "revision": "e0fa9c1eb63e3f74bcdb",
+    "url": "/movie_2020/static/js/2.ab410cae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_2020/static/js/2.ab410cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bb244042bf88303e1c9",
+    "url": "/movie_2020/static/js/main.1cfa746d.chunk.js"
+  },
+  {
+    "revision": "d0c1a4c31111951ebdd2",
+    "url": "/movie_2020/static/js/runtime-main.9b8d2b91.js"
+  }
+]);
